89d44ec4fb2ea3996775ec44704044eebad888d3
l000ps <toporoleksandr@gmail.com>
2023-06-05 10:25:55 +0300

    Stage 3/5: Game logic

--- Main.kt
+++ Main.kt
@@ -1,19 +1,25 @@
 package connectfour
 
+import kotlin.system.exitProcess
+
 var rows = 6
 var columns = 7
-
+var board = mutableListOf<MutableList<Char>>()
+var firstP = ""
+var secondP = ""
 
 fun main() {
     println("Connect Four")
     println("First player's name:")
-    val firstP = readln()
+    firstP = readln()
     println("Second player's name:")
-    val secondP = readln()
+    secondP = readln()
     setBoardDim()
     println("$firstP VS $secondP")
     println("$rows X $columns board")
-
+    board = MutableList(rows) { MutableList(columns) { ' ' } }
+    printBoard()
+    play()
 }
 
 fun setBoardDim() {
@@ -39,6 +45,69 @@ fun setBoardDim() {
             }
         } catch (e: Exception) {
             println("Invalid input")
+        }
+    }
+}
+
+
+fun printBoard() {
+    for (i in 0 until columns) {
+        print(" ${i+1}")
+    }
+    println()
+    for (i in 0 until rows) {
+        print("|")
+        for (y in 0 until columns) {
+            print("${board[i][y]}|")
+        }
+        println()
+    }
+    print("=")
+    for(i in 0 until columns - 1) {
+        print("==")
+    }
+    print("==")
+    println()
+}
+
+fun play() {
+    var isFirst = true
+    var disk = ' '
+    var column = 0
+    while (true) {
+        if (isFirst) {
+            println("$firstP's turn:")
+            disk = 'o'
+        } else {
+            println("$secondP's turn:")
+            disk = '*'
+        }
+        try {
+            val userInp = readln()
+            if (userInp == "end") {
+                println("Game over!")
+                exitProcess(0)
+            }
+            column = userInp.toInt()
+            if (column > columns || column < 1) {
+                println("The column number is out of range (1 - $columns)")
+                continue
+            } else if (board[0][column-1] != ' ') {
+                println("Column $column is full")
+                continue
+            } else {
+                for (i in rows - 1 downTo   0 ) {
+                    if (board[i][column-1] == ' ') {
+                        board[i][column-1] = disk
+                        isFirst = !isFirst
+                        printBoard()
+                        break
+                    }
+                }
+            }
+        } catch (e:Exception) {
+            println("Incorrect column number")
+            continue
         }
     }
 }
